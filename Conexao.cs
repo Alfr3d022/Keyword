e9d4fd73a55d1bcf1c22c0af26e13fd4915a4ee4
Jaime Caetano <jaimecaetano312@gmail.com>
2023-04-07 17:01:29 -0300

    Conexao

---
+++ Conexao.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace Keyword
+{
+    internal class Conexao
+    {
+        string connectionString = "Server=26.125.84.95;Database=mydatabase;Uid=root;Pwd=usbw;";
+
+        public MySqlConnection AbrirConexao()
+        {
+            MySqlConnection conexao = new MySqlConnection(connectionString);
+            conexao.Open();
+            return conexao;
+        }
+        public void FecharConexao(MySqlConnection conexao)
+        {
+            if (conexao != null && conexao.State == System.Data.ConnectionState.Open)
+            {
+                conexao.Close();
+            }
+            else
+            {
+                Console.WriteLine("A conexão já está fechada");
+            }
+        }
+
+
+
+
+
+
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,4 @@
+using MySql.Data.MySqlClient;
 namespace Keyword
 {
     public partial class Form1 : Form
@@ -9,7 +10,25 @@ namespace Keyword
 
         private void btnLogar_Click(object sender, EventArgs e)
         {
-            //teste
+            // Abre a conexão
+            MySqlConnection conexao = new Conexao().AbrirConexao();
+
+            // Cria o comando SQL
+            string query = "SELECT * FROM tab_produtos";
+            MySqlCommand command = new MySqlCommand(query, conexao);
+
+            // Executa a consulta e lê os resultados
+            MySqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                MessageBox.Show(reader.GetString(1));
+            }
+
+            // Fecha o leitor e a conexão
+            reader.Close();
+            new Conexao().FecharConexao(conexao);
+
         }
     }
 }
