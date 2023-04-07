aba939d6648dfc1f103cfed4eb036aa4bc78677c
alf <fredimpina@gmail.com>
2023-04-07 15:09:34 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+namespace Keyword
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
